4b84076cdc0b04aa475af6e8467f93df958538bb
Markus Hauru <markus@mhauru.org>
2018-10-30 16:53:06 +0200

    Many things.

--- keyboarder.py
+++ keyboarder.py
@@ -3,10 +3,13 @@ import itertools as itt
 
 
 base_note = 440
+horizontal_scale = 250
 primes = [2,3,5,7]
 distance_constants = [0,2,3,100]
-yshift_constants = [0,120,10,50]
-horizontal_scale = 250
+yshift_constants = [0,
+                    horizontal_scale*np.log2(3/2)-130,
+                    -horizontal_scale*np.log2(5/4)-110,
+                    50]
 
 
 def interval(note):
@@ -36,15 +39,21 @@ def note_position(note):
     
 
 def draw_note(note):
-    global svg_mid
+    global svg_notes, svg_pitchlines, max_harm_dist
+    harm_dist = harmonic_distance(note)
+    rel_harm_dist = 1 - harm_dist/max_harm_dist
     x, y = note_position(note)
-    svg_line = '<use href="#noteCircle" x="{:f}" y="{:f}"/>\n<use href="#pitchLine" x="{:f}"/>\n'.format(x,y,x)
-    svg_mid += svg_line
+    note_line = '<use href="#noteCircle" x="{:f}" y="{:f}" opacity="{:f}"/>\n'.format(x,y, rel_harm_dist)
+    pitchline_line = '<use href="#pitchLine" x="{:f}" opacity="{:f}"/>\n'.format(x, rel_harm_dist)
+    svg_notes += note_line
+    svg_pitchlines += pitchline_line
     return None
 
 
 def draw_interval(note, step):
-    global svg_mid
+    global svg_intervals, max_harm_dist
+    harm_dist = harmonic_distance(note)
+    rel_harm_dist = 1 - harm_dist/max_harm_dist
     x, y = note_position(note)
     if step == (1,0,0,0):
         stepname = "octaveUp"
@@ -58,8 +67,8 @@ def draw_interval(note, step):
         stepname = "fiveUp"
     elif step == (2,0,-1,0):
         stepname = "fiveDown"
-    svg_line = '<use href="#{}" x="{:f}" y="{:f}"/>\n'.format(stepname, x, y)
-    svg_mid += svg_line
+    svg_line = '<use href="#{}" x="{:f}" y="{:f}" opacity="{:f}"/>\n'.format(stepname, x, y, rel_harm_dist)
+    svg_intervals += svg_line
     return None
 
 
@@ -67,7 +76,9 @@ if __name__ == "__main__":
     max_harm_dist = 6
     max_pitch_dist = 8
 
-    svg_mid = ""
+    svg_notes = ""
+    svg_intervals = ""
+    svg_pitchlines = ""
 
     notes_to_draw = {(0,0,0,0)}
     notes_drawn = set()
@@ -79,12 +90,6 @@ if __name__ == "__main__":
             next_note = tuple(n + s for n, s in zip(note, step))
             harm_dist = harmonic_distance(next_note)
             intrvl = interval(next_note)
-            print()
-            print(note)
-            print(next_note)
-            #print(notes_to_draw)
-            #print(notes_drawn)
-            print(harm_dist, intrvl)
             if (harm_dist <= max_harm_dist and
                     (intrvl <= max_pitch_dist
                      and 1/intrvl <= max_pitch_dist)):
@@ -92,80 +97,19 @@ if __name__ == "__main__":
                     if next_note not in notes_drawn:
                         notes_to_draw.add(next_note)
         
-    svg_pre = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-
-<svg
-width="1000"
-height="500"
-viewBox="-500 -250 1000 500"
-version="1.1"
-id="svg8"
-xmlns:dc="http://purl.org/dc/elements/1.1/"
-xmlns:cc="http://creativecommons.org/ns#"
-xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-xmlns:svg="http://www.w3.org/2000/svg"
-xmlns="http://www.w3.org/2000/svg">
-
-<defs>
-
-<circle
-id="noteCircle"
-style="fill:#ac0006;fill-opacity:1;stroke:none;stroke-width:1.5;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
-r="10.0" />
-
-<path
-style="fill:none;stroke:#c7c7c7;stroke-width:3.0;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:0.5, 0.5;stroke-dashoffset:0;stroke-opacity:1"
-d="M 0,-250 V 500"
-id="pitchLine"
-/>
-
-<path
-style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="M 0,0 {:f},{:f}"
-id="octaveUp"
-/>
-
-<path
-style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="M 0,0 {:f},{:f}"
-id="octaveDown"
-/>
-
-<path
-style="fill:none;stroke:#001bac;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="m 0,0 {:f},{:f}"
-id="threeUp"
-/>
-
-<path
-style="fill:none;stroke:#001bac;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="m 0,0 {:f},{:f}"
-id="threeDown"
-/>
-
-<path
-style="fill:none;stroke:#ac5f00;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="m 0,0 {:f},{:f}"
-id="fiveUp"
-/>
-
-<path
-style="fill:none;stroke:#ac5f00;stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-d="m 0,0 {:f},{:f}"
-id="fiveDown"
-/>
-
-</defs>
-    """.format(horizontal_scale*np.log2(2/1), -yshift_constants[0],
-               horizontal_scale*np.log2(1/2), yshift_constants[0],
-               horizontal_scale*np.log2(3/2), -yshift_constants[1],
-               horizontal_scale*np.log2(2/3), yshift_constants[1],
-               horizontal_scale*np.log2(5/4), -yshift_constants[2],
-               horizontal_scale*np.log2(4/5), yshift_constants[2])
-    svg_post = """
-</svg>
-    """
-    svg = svg_pre + svg_mid + svg_post
+    with open("svg_pre.svg-part", "r") as f:
+        svg_pre = f.read()
+    svg_pre = svg_pre.format(
+        horizontal_scale*np.log2(2/1), -yshift_constants[0],
+        horizontal_scale*np.log2(1/2), yshift_constants[0],
+        horizontal_scale*np.log2(3/2), -yshift_constants[1],
+        horizontal_scale*np.log2(2/3), yshift_constants[1],
+        horizontal_scale*np.log2(5/4), -yshift_constants[2],
+        horizontal_scale*np.log2(4/5), yshift_constants[2]
+    )
+    svg_post = "\n</svg>"
+    svg = svg_pre + svg_pitchlines + svg_intervals + svg_notes + svg_post
 
     with open("keyboard_generated.svg", "w") as f:
         f.write(svg)
+
